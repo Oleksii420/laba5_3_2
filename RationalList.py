9845d76e8e9e066f0515f5701ba7a04f7faac1e2
Алексей Дидык <adidyk110@gmail.com>
2024-06-14 17:03:26 +0300

    ff

---
+++ RationalList.py
@@ -0,0 +1,59 @@
+from Rational import Rational
+
+
+class RationalList:
+    def __init__(self):
+        self.data = []
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __setitem__(self, index, value):
+        if isinstance(value, Rational):
+            self.data[index] = value
+        elif isinstance(value, int):
+            self.data[index] = Rational(value)
+        else:
+            raise ValueError()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __add__(self, other):
+        if isinstance(other, RationalList):
+            return RationalList.from_list(self.data + other.data)
+        elif isinstance(other, Rational):
+            return RationalList.from_list(self.data + [other])
+        elif isinstance(other, int):
+            return RationalList.from_list(self.data + [Rational(other)])
+
+
+    def __iadd__(self, other):
+        if isinstance(other, RationalList):
+            self.data.extend(other.data)
+        elif isinstance(other, Rational):
+            self.data.append(other)
+        elif isinstance(other, int):
+            self.data.append(Rational(other))
+
+        return self
+
+    def append(self, value):
+        if isinstance(value, Rational):
+            self.data.append(value)
+        elif isinstance(value, int):
+            self.data.append(Rational(value))
+
+
+
+    def from_list(cls, lst):
+        instance = cls()
+        for item in lst:
+            instance.append(item)
+        return instance
+
+    def sum(self):
+        total = Rational(0)
+        for item in self.data:
+            total += item
+        return total
---
+++ read_rational_file.py
@@ -0,0 +1,21 @@
+from Rational import Rational
+from RationalList import RationalList
+
+def read_rational_file(filename):
+    rational_list = RationalList()
+    with open(filename, 'r') as file:
+        for line in file:
+            numbers = line.split()
+            for num in numbers:
+                if '/' in num:
+                    n, d = map(int, num.split('/'))
+                    rational_list.append(Rational(n, d))
+                else:
+                    rational_list.append(int(num))
+    return rational_list
+
+
+file_list = ['input01.txt', 'input02.txt', 'input03.txt']
+for filename in file_list:
+    r_list = read_rational_file(filename)
+    print(f"Сума чисел в {filename}: {r_list.sum()}")
